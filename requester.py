6dfb08055d31fe50718021dfed39fc41662d0863
Nishaanth Narayanan <nish6474@sun.cs.tamu.edu>
2014-03-26 18:16:47 -0500

    Working Copy of Requester with Main commented out

--- requester.py
+++ requester.py
@@ -14,18 +14,12 @@ class Requester:
 		self.title = title
 		self.description = description
 		self.keywords = keywords
-		
-	# def insert_text(text):
-		# self.content = QuestionContent()
-		# self.content.append_field("Text", text)
-		
-	# def insert_image(img):
-		# self.content = QuestionContent(binary = img)
-		
-	def create_question(self, question_text, title_text="Provide Question(s) Based On The Text Below"):	
+
 	
+	def create_question(self, question_text, typeflag, title_text="Provide Question(s) Based On The Text Below"):	
 		# BUILD OVERVIEW
-		self.overview = Overview(title = "Instructions")
+		self.overview = Overview()
+		self.overview.append_field("Title",title_text)
 		self.overview.append(FormattedContent("""
 			<ul>
 				<li><font size="1" color="gray">Read the paragraph and write down one or more questions (max 5)</font></li>
@@ -36,9 +30,13 @@ class Requester:
 			
 
 		# Question 1
-		qc1 = QuestionContent()
-		qc1.append_field("Text",question_text)
+		qc1=QuestionContent()
+		if typeflag == "image":
+			qc1.append(Binary("image","jpg",question_text,title_text))
+		else:
+			qc1.append_field("Text",question_text)
 
+		print "here"
 		ans1 = FreeTextAnswer()
 
 		self.q1 = Question(identifier="Question", content=qc1, answer_spec=AnswerSpecification(ans1))
@@ -55,23 +53,25 @@ class Requester:
 
 	def launch_hit(self):
 		# Creating the HIT
-
 		self.mtc.create_hit(questions=self.question_form, max_assignments=2, title=self.title, description=self.description, keywords=self.keywords, duration=60*5, reward=0.05)
 
 	def account_balance(self):
 		print self.mtc.get_account_balance()
 
-if __name__ == "__main__":
-	if (len(sys.argv) < 1):
-		print "Usage: python requester.py <name_of_file_with_text>"
-		sys.exit(0)
-	req = Requester()
-	req.initialize_request_details("Read the given material and provide question(s)", 
-		"Read the given paragraph and come up with logical, relevant questions that can be used in a study review session",
-		"education, study, school")
-	req.create_question(title_text="Question This", question_text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nisi elit, rutrum nec laoreet et, rhoncus condimentum tellus. Donec quam est, condimentum ullamcorper sapien id, porta volutpat mauris. Pellentesque at elit euismod, congue mi non, tristique lorem. Morbi ornare, turpis sed dapibus dignissim, leo dolor aliquet nulla, vitae auctor sapien odio id mi. Proin ut rutrum mi, sit amet cursus felis. Aliquam in nulla sed arcu vestibulum auctor eu eget metus. Curabitur dolor eros, eleifend quis varius quis, malesuada quis ipsum. Praesent elementum velit eu nibh ultricies fringilla. In suscipit facilisis erat, sit amet tempor mauris dapibus et. Duis faucibus eros ut urna interdum gravida. Integer cursus enim cursus accumsan posuere. Cras laoreet tristique quam ut facilisis. Mauris congue nibh eu tellus consequat eleifend. Aenean viverra eros in risus euismod, sit amet lobortis augue venenatis.")
-	req.build_question_form()
-	req.launch_hit()
-	req.account_balance()
+# if __name__ == "__main__":
+	# if (len(sys.argv) < 3):
+		# print "Usage: python requester.py <typeflag> <text_to_create_question/url_of_image>"
+		# sys.exit(0)
+	# typeflag = sys.argv[1]
+	# req = Requester()
+	# req.initialize_request_details("Read the given material and provide question(s)", 
+		# "Read the given paragraph and come up with logical, relevant questions that can be used in a study review session",
+		# "education, study, school")
+		
+	# req.create_question(title_text="Question This", typeflag=typeflag, question_text="https://i.imgur.com/UWnTcNs.jpg?1")
+	# #req.create_question(title_text="Question This", typeflag=typeflag,question_text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nisi elit, rutrum nec laoreet et, rhoncus condimentum tellus. Donec quam est, condimentum ullamcorper sapien id, porta volutpat mauris. Pellentesque at elit euismod, congue mi non, tristique lorem. Morbi ornare, turpis sed dapibus dignissim, leo dolor aliquet nulla, vitae auctor sapien odio id mi. Proin ut rutrum mi, sit amet cursus felis. Aliquam in nulla sed arcu vestibulum auctor eu eget metus. Curabitur dolor eros, eleifend quis varius quis, malesuada quis ipsum. Praesent elementum velit eu nibh ultricies fringilla. In suscipit facilisis erat, sit amet tempor mauris dapibus et. Duis faucibus eros ut urna interdum gravida. Integer cursus enim cursus accumsan posuere. Cras laoreet tristique quam ut facilisis. Mauris congue nibh eu tellus consequat eleifend. Aenean viverra eros in risus euismod, sit amet lobortis augue venenatis.")
+	# req.build_question_form()
+	# req.launch_hit()
+	# req.account_balance()
 	
 	
